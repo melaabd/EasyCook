3699b51ee75659248227ae43fb05ab39d2a84bc2
Mohamed Abdelmaugod <mohamd.elaabd@gmail.com>
2022-01-02 15:55:37 +0400

    optimization

--- EasyCook/Extensions/UIViewExtension.swift
+++ EasyCook/Extensions/UIViewExtension.swift
@@ -6,20 +6,24 @@
 //
 
 import UIKit
-//extension UIView {
-//
-//    /// return views that anabled to have skeleton
-//    @objc var subviewsSkeletonables: [UIView] {
-//        return subviewsToSkeleton.filter { $0.isSkeletonable }
-//    }
-//
-//    @objc var subviewsToSkeleton: [UIView] {
-//        return subviews
-//    }
-//}
+extension UIView {
+
+    /// return views that anabled to have skeleton
+    @objc var subviewsSkeletonables: [UIView] {
+        return subviewsToSkeleton.filter { $0.isSkeletonable }
+    }
+
+    
+    /// return subviews of view
+    @objc var subviewsToSkeleton: [UIView] {
+        return subviews
+    }
+}
 
 extension UITableViewCell {
 
+    
+    /// return subviews of contentView of cell
     override var subviewsToSkeleton: [UIView] {
         return contentView.subviews
     }
